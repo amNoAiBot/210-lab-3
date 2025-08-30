28a5575960b6858a9f29562f3702534acd1268c3
Sunny Anand <sanand853@insite.4cd.edu>
2025-08-30 00:36:15 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+/**********************************************************************
+* Name: Sunny Anand
+*
+* Course: COMSC-210: Program Design & Data Structures by Prof. Mark Dietrich
+*
+* Assignment: Lab 3: Hello World
+*
+* IDE/Compiler(s) used: VSC/G++
+*
+* Description:
+* Write a simple cpp file & push it to GitHub 😺
+**********************************************************************/
+
+
+
+
+/*********************************************************************
+*               Importing C++ Built-in Library Functions
+**********************************************************************/
+#include <iostream>
+using namespace std;
+
+
+
+
+
+/*********************************************************************
+*
+*                           The main() function
+*
+**********************************************************************/
+
+int main() {
+
+    cout << "Hello World!";
+    
+    return 0;
+
+}
